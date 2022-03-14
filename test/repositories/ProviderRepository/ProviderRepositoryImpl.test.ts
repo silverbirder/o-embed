48430e6e74f3c2061a9b2f68d88e0ad69d6aa0de
shibamoto <silverbirder@gmail.com>
2022-03-14 21:39:11 +0900

    Add mock code

--- test/repositories/ProviderRepository/ProviderRepositoryImpl.test.ts
+++ test/repositories/ProviderRepository/ProviderRepositoryImpl.test.ts
@@ -1,13 +1,31 @@
+import { expect } from '@open-wc/testing';
+import { SinonStub, stub } from 'sinon';
 import { ProviderRepository } from '../../../src/repositories/index.js';
 
-describe('ProviderRepositoryImpl', () => {
-  it('invoke', async () => {
-    // Arrange
-    const repository = new ProviderRepository('');
-    // Act
-    const a = await repository.invoke('');
+const jsonOk = (body: any) => {
+  const mockResponse = new window.Response(JSON.stringify(body), {
+    status: 200,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  return Promise.resolve(mockResponse);
+};
 
-    // Assert
-    console.log(a);
+describe('ProviderRepositoryImpl', () => {
+  let fetch: SinonStub<any>;
+  beforeEach(() => {
+    fetch = stub(window, 'fetch');
+    fetch.onCall(0).returns(jsonOk([]));
+  });
+  afterEach(() => {
+    fetch.restore();
+  });
+  it('invoke', async () => {
+    const repository = new ProviderRepository('');
+
+    const actual = await repository.invoke('');
+
+    expect(actual).to.be.length(0);
   });
 });
